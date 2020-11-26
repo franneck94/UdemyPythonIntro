f21562b720e1c078c6167e48cc43fa4a7929595c
franneck94 <schaffranek.jan@web.de>
2020-11-26 11:34:14 +0100

    Updated modules code

--- Chapter2_Python/UsingModules.py
+++ Chapter2_Python/UsingModules.py
@@ -1,34 +1,17 @@
-# import MyModule # Alternative
+# import MyModule  # Alternative 1
 
+# list1 = [-2, 1, 3]
+# list1_max = MyModule.list_max(list1)
+# print(list1_max)
 
-# l1 = [-2, 1, 2, -10, 22, -10]
-# l1_max = MyModule.list_max(l1)
-# print(l1_max)
+# from MyModule import list_max   # Alternative 2
 
-# l2 = [-20, 123, 112, -10, 22, -120]
-# l2_max = MyModule.list_max(l2)
-# print(l2_max)
+# list1 = [-2, 1, 3]
+# list1_max = list_max(list1)
+# print(list1_max)
 
+# import MyModule as MM
 
-# from MyModule import list_max # Alternative 2
-
-
-# l1 = [-2, 1, 2, -10, 22, -10]
-# l1_max = list_max(l1)
-# print(l1_max)
-
-# l2 = [-20, 123, 112, -10, 22, -120]
-# l2_max = list_max(l2)
-# print(l2_max)
-
-
-import MyModule as mm  # Alternative 3
-
-
-l1 = [-2, 1, 2, -10, 22, -10]
-l1_max = mm.list_max(l1)
-print(l1_max)
-
-l2 = [-20, 123, 112, -10, 22, -120]
-l2_max = mm.list_max(l2)
-print(l2_max)
+# list1 = [-2, 1, 3]
+# list1_max = MM.list_max(list1)
+# print(list1_max)
